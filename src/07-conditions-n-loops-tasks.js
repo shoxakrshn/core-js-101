ad5b0a4519dee09ecb54c23ce5e64ff8db540a30
shoxa <sllx.ok@gmail.com>
2023-05-31 00:38:59 +0500

    feat: solve 'evaluateTicTacToePosition' in 07-conditions-n-loops-tasks.js

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -507,8 +507,42 @@ function getMatrixProduct(m1, m2) {
  *    [    ,   ,    ]]
  *
  */
-function evaluateTicTacToePosition(/* position */) {
-  throw new Error('Not implemented');
+function evaluateTicTacToePosition(position) {
+  if (
+    position[0][2] !== undefined
+    && position[0][2] === position[1][1]
+    && position[0][2] === position[2][0]
+  ) {
+    return position[0][2];
+  }
+
+  if (
+    position[0][0] !== undefined
+    && position[0][0] === position[1][1]
+    && position[0][0] === position[2][2]
+  ) {
+    return position[0][0];
+  }
+
+  for (let i = 0; i < 3; i += 1) {
+    if (
+      position[i][0] !== undefined
+      && position[i][0] === position[i][1]
+      && position[i][0] === position[i][2]
+    ) {
+      return position[i][0];
+    }
+
+    if (
+      position[0][i] !== undefined
+      && position[0][i] === position[1][i]
+      && position[0][i] === position[2][i]
+    ) {
+      return position[0][i];
+    }
+  }
+
+  return undefined;
 }
 
 
